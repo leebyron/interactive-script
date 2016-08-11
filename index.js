7d7db998bc045705d4ee9c6f708866ccf0915caf
Lee Byron <lee@leebyron.com>
2016-08-10 23:46:46 -0700

    yN options

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function runInteractiveScript(fn) {
     output: process.stdout
   })
 
+  /**
+   * statement is formatted if not a string.
+   * key is the second argument to readline.Interface#write
+   */
   function say(statement, key) {
     var output = statement == null || typeof statement === 'string' ?
       statement :
@@ -14,18 +18,39 @@ function runInteractiveScript(fn) {
     rl.write(output + '\n', key)
   }
 
-  function ask(query) {
-    return new Promise(resolve => rl.question(query, resolve))
+  /**
+   * query is prompted on the same line
+   * Yn: return boolean defaulting to true
+   * yN: return boolean defaulting to false
+   */
+  function ask(query, yN) {
+    return new Promise(function (resolve) {
+      if (yN === 'Yn') {
+        query += '[Yn] ';
+      } else if (yN) {
+        query += '[yN] ';
+      }
+      rl.question(query, function (answer) {
+        if (yN === 'Yn') {
+          resolve(answer[0] !== 'n' && answer[0] !== 'N')
+        } else if (yN) {
+          resolve(answer[0] === 'y' || answer[0] === 'Y')
+        } else {
+          resolve(answer)
+        }
+      })
+    })
   }
 
   fn(say, ask).then(
-    function () {
+    function (code) {
       rl.close()
+      process.exit(code || 0)
     },
     function (error) {
       rl.close()
       console.error(error)
-      process.exitCode = process.exitCode || 1
+      process.exit(process.exitCode || 1)
     }
   )
 
